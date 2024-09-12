58dda70f47aff4880e749e6fc72827b3240d7ac3
Hemachandiran <83321708+Hemachandirant@users.noreply.github.com>
2024-09-12 11:21:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,31 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
+
 app = FastAPI()
-# Mount the static directory to serve static files
-app.mount("/", StaticFiles(directory=".", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def index():
+async def index(request: Request):
     print('Request for index page received')
-    with open("login.html", "r") as file:
-        return HTMLResponse(content=file.read(), status_code=200)
+    return templates.TemplateResponse('index.html', {"request": request})
 
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = 'favicon.ico'
+    file_path = './static/' + file_name
+    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
 
-
+@app.post('/hello', response_class=HTMLResponse)
+async def hello(request: Request, name: str = Form(...)):
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8008)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+
